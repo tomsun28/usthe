6698c51f672591b7a3cbffe0b5dcabd091a4c6f3
tomsun28 <tomsun28@outlook.com>
2018-05-07 15:18:09 +0800

    更改生产环境api的ip为域名

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://116.196.81.106/api/',
+  apiBaseUrl: 'http://tom.usthe.com/api/',
   appName: 'usThe',
   version: '1.0.0'
 };
